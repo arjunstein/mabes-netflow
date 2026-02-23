75110173f8c3cf060215bb6d5bd25b47d9c0aa14
arjnstn <arjun.gnw09@gmail.com>
2026-02-23 08:42:36 +0700

    🔥 chore(btn): disable temporary download csv

--- resources/views/components/chart-card.blade.php
+++ resources/views/components/chart-card.blade.php
@@ -11,7 +11,7 @@
         </h3>
 
         <!-- Download Button -->
-        <div class="relative" x-data="{ open: false }">
+        <div class="relative" x-data="{ open: false }" x-cloak x-transition.origin.top.right>
             <button @click="open = !open" class="p-2 rounded hover:bg-gray-100">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-5">
